bc650a9349fef5187a5a88c69b427eb57026dc59
KevinKobori <kevin.kobori@hotmail.com>
2023-10-02 08:08:37 -0300

    Removed old folders

--- nasted_navigation/flutter_modular_nasted_navigation_poc/lib/modules/profile/profile_settings/profile_settings.module.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_modular/flutter_modular.dart';
-import 'package:flutter_modular_nasted_navigation_poc/modules/profile/profile_settings/profile_settings.page.dart';
-
-class ProfileSettingsModule extends Module {
-  @override
-  void routes(r) {
-    r.child('/', child: (context) => const ProfileSettingsPage());
-  }
-}
--- nasted_navigation/flutter_modular_nasted_navigation_poc/lib/modules/profile/profile_verification/profile_verification.module.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_modular/flutter_modular.dart';
-import 'package:flutter_modular_nasted_navigation_poc/modules/profile/profile_verification/profile_verification.page.dart';
-
-class ProfileVerificationModule extends Module {
-  @override
-  void routes(r) {
-    r.child('/', child: (context) => const ProfileVerificationPage());
-  }
-}

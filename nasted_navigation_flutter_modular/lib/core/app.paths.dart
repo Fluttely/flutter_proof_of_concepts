4e132881e07b392fdb50eb828dd9001d4d2d3727
KevinKobori <kevin.kobori@hotmail.com>
2023-10-02 07:34:44 -0300

    Changed repository tree logic

---
+++ nasted_navigation_flutter_modular/lib/core/app.paths.dart
@@ -0,0 +1,21 @@
+/// AppCurrentPaths
+class AppPaths {
+  /// profile
+  static const String profile = '/profile';
+
+  /// profile Settings
+  static const String settings = '/settings';
+
+  /// profile Verification
+  static const String verification = '/verification';
+
+  /// Find
+  static const String find = '/find';
+
+  /// Find Person
+  static const String person = '/person';
+  static const String details = '/details';
+
+  /// Find Event
+  static const String event = '/event';
+}
